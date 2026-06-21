1718f706a65fe83018e8a9e4fc0b6cf4acdd8fd7
Sahil kumar <148351667+sahilk1012@users.noreply.github.com>
2026-06-21 11:37:02 +0530

    Add files via upload

---
+++ 05_Loops/21_While_Do_while.js
@@ -0,0 +1,30 @@
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 10
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+// special condition of the do while
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+// It will print the "score is  11" after that it will stop..its condition is that it print first then then check the condition
---
+++ 05_Loops/24_For_Each.js
@@ -0,0 +1,41 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+//For each uses the callBack function.
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe())
+// coding.forEach(printMe)//both methods are right
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
